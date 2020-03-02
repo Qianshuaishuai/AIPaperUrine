79153338684d7ac2ff9438a822b81869ccea63bd
QianDashuai <491787891@qq.com>
2020-03-02 20:35:51 +0800

    [add]增加设备提醒和提醒详情

---
+++ app/src/main/java/com/babyraising/aipaperurine/ui/info/DeviceRemindActivity.java
@@ -0,0 +1,18 @@
+package com.babyraising.aipaperurine.ui.info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyraising.aipaperurine.R;
+import com.babyraising.aipaperurine.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_device_remind)
+public class DeviceRemindActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/babyraising/aipaperurine/ui/info/RemindDetailActivity.java
@@ -0,0 +1,19 @@
+package com.babyraising.aipaperurine.ui.info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyraising.aipaperurine.R;
+import com.babyraising.aipaperurine.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_remind_detail)
+public class RemindDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
