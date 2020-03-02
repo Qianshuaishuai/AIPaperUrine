fe83885501028e4294a13bba06da3753d7fe5ac0
QianDashuai <491787891@qq.com>
2020-03-02 22:01:12 +0800

    [add]增加报警设置，睡姿页面

---
+++ app/src/main/java/com/babyraising/aipaperurine/ui/info/CallSettingActivity.java
@@ -0,0 +1,19 @@
+package com.babyraising.aipaperurine.ui.info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyraising.aipaperurine.R;
+import com.babyraising.aipaperurine.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_call_setting)
+public class CallSettingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/babyraising/aipaperurine/ui/info/SleepPostureActivity.java
@@ -0,0 +1,41 @@
+package com.babyraising.aipaperurine.ui.info;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.babyraising.aipaperurine.R;
+import com.babyraising.aipaperurine.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+@ContentView(R.layout.activity_sleep_posture)
+public class SleepPostureActivity extends BaseActivity {
+
+    @ViewInject(R.id.middle_iv)
+    private ImageView ivMiddle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+        WindowManager wm = (WindowManager) this
+                .getSystemService(Context.WINDOW_SERVICE);
+        int height = wm.getDefaultDisplay().getHeight();
+
+        int commonHeight = 1776;
+        int scale = height / commonHeight;
+        int offsetHeight = 400 * scale;
+
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) ivMiddle.getLayoutParams();
+        params.topMargin = offsetHeight;
+        ivMiddle.setLayoutParams(params);
+    }
+}
