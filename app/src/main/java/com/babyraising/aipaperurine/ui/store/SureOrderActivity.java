6c4c0f777a78b3afa032ceb934cc5c4411ce2299
QianDashuai <491787891@qq.com>
2020-03-04 22:01:07 +0800

    [add]添加确认订单详情

---
+++ app/src/main/java/com/babyraising/aipaperurine/ui/store/SureOrderActivity.java
@@ -0,0 +1,18 @@
+package com.babyraising.aipaperurine.ui.store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyraising.aipaperurine.R;
+import com.babyraising.aipaperurine.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_sure_order)
+public class SureOrderActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
