e2df3e23f4335e025a4b74c4e00bdb6cafc090a3
QianDashuai <491787891@qq.com>
2020-03-05 14:31:48 +0800

    [add]增加地址编辑页面

---
+++ app/src/main/java/com/babyraising/aipaperurine/ui/address/AddressEditActivity.java
@@ -0,0 +1,17 @@
+package com.babyraising.aipaperurine.ui.address;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyraising.aipaperurine.R;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_address_edit)
+public class AddressEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
