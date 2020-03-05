1c64f0923aab1a6ff7b66593b005683752d5186b
QianDashuai <491787891@qq.com>
2020-03-05 16:38:20 +0800

    [add]增加设备信息详情页面

---
+++ app/src/main/java/com/babyraising/aipaperurine/message/DeviceMessageActivity.java
@@ -0,0 +1,18 @@
+package com.babyraising.aipaperurine.message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyraising.aipaperurine.R;
+import com.babyraising.aipaperurine.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_device_message)
+public class DeviceMessageActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
