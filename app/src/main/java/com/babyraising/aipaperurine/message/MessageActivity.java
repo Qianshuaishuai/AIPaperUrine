d0819a007076c4c6ce72894226a01cef64d44931
QianDashuai <491787891@qq.com>
2020-03-05 16:30:31 +0800

    [add]增加消息中心页面

---
+++ app/src/main/java/com/babyraising/aipaperurine/message/MessageActivity.java
@@ -0,0 +1,18 @@
+package com.babyraising.aipaperurine.message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyraising.aipaperurine.R;
+import com.babyraising.aipaperurine.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_message)
+public class MessageActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
