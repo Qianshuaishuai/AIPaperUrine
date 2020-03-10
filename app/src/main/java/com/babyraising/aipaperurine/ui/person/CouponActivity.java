d2f7d3d8865dd0f58d566109353f37bd8a935e12
QianDashuai <491787891@qq.com>
2020-03-10 10:47:28 +0800

    [add]增加优惠券页面

---
+++ app/src/main/java/com/babyraising/aipaperurine/ui/person/CouponActivity.java
@@ -0,0 +1,18 @@
+package com.babyraising.aipaperurine.ui.person;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.babyraising.aipaperurine.R;
+import com.babyraising.aipaperurine.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_coupon)
+public class CouponActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
